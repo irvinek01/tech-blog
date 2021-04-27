6adacd81efb795cdaa5357928babc37c45ef3f90
Arvin John Bautista <arvinyoung01@gmail.com>
2021-04-27 10:17:13 -0700

    seeds.js and json format datas

---
+++ seeds/seed.js
@@ -0,0 +1,20 @@
+const { User, Post, Comment } = require("../models");
+const sequelize = require("../config/connection");
+
+const userData = require("./dataUsers.json");
+const postData = require("./dataPosts.json");
+const commentData = require("./dataComments.json");
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+  await Post.bulkCreate(postData);
+  await Comment.bulkCreate(commentData);
+
+  process.exit(0);
+};
+
+seedDatabase();
